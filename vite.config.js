360b6baa9a20e77a0b45ff138d68916a2b517a5b
yaroslav-shevchuk <shevchyk.yar@e-u.edu.ua>
2025-11-14 22:41:29 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD:vite.config.js
-  base: "/praktychna9/", 
-})
-=======
-  // Встановіть тут назву вашого репозиторію на GitHub
-  base: "/praktychna9/", 
-})
->>>>>>> 22bac9b438071dd18bcc53e215e6ae5d2b4cdab8:my-react-app/vite.config.js
+  base: "/praktychna9/",
+});
